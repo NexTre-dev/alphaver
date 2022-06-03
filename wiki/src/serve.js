dab21df49332e9fb2d756c9b584a848076c9a3f4
pr1ncess_emily <b3nb0t5000@gmail.com>
2022-06-03 14:06:05 -0600

    adding error handling to server

--- wiki/src/serve.js
+++ wiki/src/serve.js
@@ -2,11 +2,18 @@ import serveStatic from "serve-static";
 import config from './config.json' assert {type: 'json'};
 import http from 'http';
 import fs from 'fs';
+import { exit } from "process";
 
 const runServer = () => {
     const serveRoot = serveStatic(config.outputDirectory, {
         index: 'index.html'
     });
+
+    if (!fs.existsSync(config.outputDirectory)) {
+        console.error('Site directory does not exist. Have you built the site yet?');
+        exit();
+    }
+
     const server = http.createServer((req, res) => {
         serveRoot(req, res, (req, res) => {
             return;
@@ -14,6 +21,12 @@ const runServer = () => {
     });
 
     server.listen(8080);
+    if (server.listening) {
+        console.log('Local server is running and listening on port 8080.\nGo to \'http://localhost:8080\' to view the wiki site.');
+    } else {
+        console.error('Failed to create local server');
+        exit();
+    }
 }
 
 runServer();

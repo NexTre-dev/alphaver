eb8b9f08c68acb49d360132864928588199917c6
pr1ncess_emily <b3nb0t5000@gmail.com>
2022-06-03 13:30:41 -0600

    moving build code into src

--- wiki/src/build.js
+++ wiki/src/build.js
@@ -1,5 +1,4 @@
 // Imports
-import fs from 'fs';
 import process from 'process';
 import * as url from 'url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -7,11 +6,11 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 import PageCreator from './PageCreator.js';
 
 // Setting project root
-const __baseDir = __dirname.replace(/\/wiki/gi, "");
+const __baseDir = __dirname.replace(/\/wiki.*/gi, "");
 
 // Setting config options
 import config from './config.json' assert {type: 'json'};
-const outputDir = config.outputDirectory  ? '.' + config.outputDirectory : "./site";
+const outputDir = config.outputDirectory  ? config.outputDirectory : "../site";
 
 const build = () => {
     let buildTimeStart = process.hrtime();
